1502cd90c93dcc235848ad71f8f7265abc9f1241
unknown <alzayon@gmail.com>
2017-09-24 23:52:33 +0800

    Started with api

---
+++ api/src/main/kotlin/alexis/exam/android/com/freelancer/Main.kt
@@ -0,0 +1,10 @@
+package alexis.exam.android.com.freelancer
+
+import alexis.exam.android.com.freelancer.api.ApiClient
+
+/**
+ * Created by alzayon on 9/24/2017.
+ */
+fun main(args: Array<String>) {
+    val retrofit = ApiClient.getClient()
+}
